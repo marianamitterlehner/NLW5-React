740d5b8c3b9ceb077aba8aeb0fa249b690714beb
Mariana Mitterlehner <mariana.m.miguel@gmail.com>
2021-04-24 21:27:06 -0300

    corrigindo erro e update 1 do ep 3

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,22 +1,100 @@
+import {GetStaticProps} from 'next';
+import {format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
-export default function Home(props) {
-  return (
-    <div>
-      <h1> Home</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </div>
+import styles from './home.module.scss';
 
-  )
+/**Tipagem */
+type Episode = {
+  /**array de objetos em json */
+    id: string;      
+    title:string;
+    members:string;
+    publishedAt: string;
+    thumbnail:string;
+    description:string;
+    url:string;
+    type: string;
+    duration: number; 
+    durationString:string;
+}
+type HomeProps = {
+  /** array de episodes */
+  latestEpisodes: Array<Episode>;
+  allEpisodes: Array<Episode>;
 }
 
-export async function getStaticProperties() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  return (
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}> 
+        <h2> Ultimos Lancamentos </h2>
 
-  return(
-    props:{
-      episodes: data,
-    },
-    revalidate: 60 * 60 * 8,
+        <ul>
+          {latestEpisodes.map(episode =>{
+            return (
+              <li key={episode.id}>
+                <img src={episode.thumbnail} alt={episode.title}/>
+                <div className={styles.episodesDetails}> 
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt} </span>
+                  <span>{episode.durationString}</span>
+                </div>
+                <button type="button"> 
+                  <img src="/play-green.svg" alt="play" />
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+
+
+      </section>
+    </div>
   )
+}
+ 
+/** getStaticProps - SSG(static-site-generation) */
+export const getStaticProps: GetStaticProps = async () => {
+  const {data} = await api.get('episodes', {
+    params: {
+      _limit: 5,
+      _sort:'published_at',
+      _order: 'desc'
+    }
+  })
+  /** ?_limit=12&_sort=published_at&_order=desc  -> como eu quero que esses dados retornem pela url */
+  
+  /** formatar os dados que vem do backend */
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,      
+      title: episode.title,
+      members:  episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      thumbnail: episode.thumbnail ,
+      description: episode.description,
+      url: episode.file.url,
+      type: episode.file.type,
+      duration: Number(episode.file.duration),
+      durationString: convertDurationToTimeString(Number(episode.file.duration)), 
+    };
+  })
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+  
+  return{
+    props:{
+      latestEpisodes,
+      allEpisodes,
+    },
+    /**de quanto em quanto tempo a pagina sera atualizada */
+    revalidate: 60 * 60 * 8,
+  }
 }
---
+++ podcastr/src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,13 @@
+
+export function convertDurationToTimeString(duration: number){
+  const hour = Math.floor(duration / 3600);  
+  const minute = Math.floor((duration % 3600) / 60);
+  const seconds = Math.floor(duration % 60);
+
+  const time = [hour, minute, seconds].map(s => String(s).padStart(2, '0')).join(':'); 
+  return time;
+}
+/**60 segundos x 60 minutos = 3600 segundos. 
+  Então uma hora é composta de 3600 segundos. 
+  Então, para fazer este cálculo basta dividir 
+  a quantidade de segundos por 3600 e você terá o resultado. */
